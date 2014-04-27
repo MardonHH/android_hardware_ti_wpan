715d98ddc30ee4cdfa43b3828a733be579abc1da
Mardon <markusmenzel@gmail.com>
2014-04-27 03:18:19 +0200

    Update ti-wpan-fm-products.mk
    Added all parts from original TI FM script

--- ti-wpan-fm-products.mk
+++ ti-wpan-fm-products.mk
@@ -1,9 +1,12 @@
 # This file lists the software specific to WiLink connectivity chip
 # FM
-PRODUCT_PACKAGES += \
-    kfmapp \
+PRODUCT_PACKAGES += uim-sysfs \
+    bt_sco_app \
+    kfmapp     \
+    BluetoothSCOApp \
     FmRxApp \
+    FmTxApp \
     FmService \
-    libfmradio \
+    Libfmradio \
     fmradioif \
     com.ti.fm.fmradioif.xml
